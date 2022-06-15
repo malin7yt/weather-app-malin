30cbff76d89852dae1ab283d6c29be808d83394f
malin7yt <MALIN7YT@GMAIL.COM>
2022-06-14 20:14:37 -0400

    Changed to imperial wind description

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let weekDayTime = document.querySelector("#weekDayTime");
 
 let date = now.getDate();
 let hours = now.getHours();
+if(hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = now.getMinutes();
 if (minutes < 10) {
   minutes = `0${minutes}`;
@@ -68,7 +71,6 @@ let forecast = response.data.daily;
 // search
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "d04e13c2e5fb57ad9c255def5985a9d2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
